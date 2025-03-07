d9161bfd6df8fec2dc4e76fdf1ca7a5eae13585c
rashid-r <rashidr0006@gmail.com>
2025-03-07 15:49:55 +0530

    Time: 0 ms (100%), Space: 28.9 MB (69.79%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int Divide(int dividend, int divisor) {
+        if(dividend == int.MinValue && divisor == -1){
+            return int.MaxValue;
+        }
+        return dividend / divisor;
+    }
+}
