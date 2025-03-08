e8514d155af4c723c211d91e02a0346c2b054fa7
rashid-r <rashidr0006@gmail.com>
2025-03-08 10:46:33 +0530

    Time: 22 ms (20.39%), Space: 53.7 MB (5.11%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,6 +1,10 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        return nums.GroupBy(p=>p).Where(n=>n.Count()==1).Select(g=>g.Key).FirstOrDefault();
+        
+        return nums.GroupBy(p=>p)
+        .Where(n=>n.Count()==1)
+        .Select(g=>g.Key)
+        .FirstOrDefault();
      
     }
 }
