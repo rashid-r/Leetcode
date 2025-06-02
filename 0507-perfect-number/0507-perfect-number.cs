d8ae0e72346179c22b0d0b8b80e2eccb4ad16c7a
rashid-r <rashidr0006@gmail.com>
2025-06-02 10:45:40 +0530

    Time: 1100 ms (40.54%), Space: 30.8 MB (91.89%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool CheckPerfectNumber(int num) {
+        if (num <= 1) return false;
+
+        int sum = 1;
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == num;
+    }
+}
