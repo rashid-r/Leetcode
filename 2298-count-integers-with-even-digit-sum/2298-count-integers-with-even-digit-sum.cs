1d10e54926408b2f3f00289dcb59ec137c714010
rashid-r <rashidr0006@gmail.com>
2025-06-03 11:03:54 +0530

    Time: 27 ms (9.09%), Space: 36.1 MB (6.82%) - LeetHub

--- 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.cs
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.cs
@@ -1,13 +1,17 @@
 public class Solution {
     public int CountEven(int num) {
         
-        var result = new List<int>();
-        List<int> list = Enumerable.Range(1,num).ToList();
-        foreach(int l in list){
-            var sum = l.ToString().Select(a=> int.Parse(a.ToString())).Sum();
-            if(sum%2==0)
-            result.Add(sum);
-        }
+        // var result = new List<int>();
+        // List<int> list = Enumerable.Range(1,num).ToList();
+        // foreach(int l in list){
+        //     var sum = l.ToString().Select(a=> int.Parse(a.ToString())).Sum();
+        //     if(sum%2==0)
+        //     result.Add(sum);
+        // }
+        var result = Enumerable.Range(1, num)
+            .Select(n => n.ToString().Select(c => int.Parse(c.ToString())).Sum()) 
+            .Where(sum => sum % 2 == 0) 
+            .ToList();
         return result.Count();
     }
 }
