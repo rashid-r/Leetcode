5e7d19cc59f81cd05aec34a1a4d1ad93ae67cad4
rashid-r <rashidr0006@gmail.com>
2025-06-09 22:24:40 +0530

    Time: 7 ms (24.44%), Space: 42.1 MB (26.67%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cs
+++ 0520-detect-capital/0520-detect-capital.cs
@@ -1,22 +1,5 @@
 public class Solution {
-    public bool DetectCapitalUse(string word)
-{
-    if (word == word.ToUpper())
-    {
-        return true; 
+    public bool DetectCapitalUse(string word) {
+        return word == word.ToUpper()||char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower()||word == word.ToLower() ? true : false;
     }
-    else if (word == word.ToLower())
-    {
-        return true; 
-    }
-    else if (char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower())
-    {
-        return true; 
-    }
-    else
-    {
-        return false;
-    }
-}
-
 }
