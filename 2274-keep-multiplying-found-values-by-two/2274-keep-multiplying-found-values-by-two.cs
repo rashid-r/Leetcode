e4a59c0a1f1fb293b4c7febec11468ebfc1023ff
rashid-r <rashidr0006@gmail.com>
2025-06-10 00:39:36 +0530

    Time: 0 ms (100%), Space: 44 MB (53.33%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cs
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cs
@@ -1,7 +1,8 @@
 public class Solution {
     public int FindFinalValue(int[] nums, int original) {
-        while (nums.Contains(original))
-        original *= 2;
-        return original;
+        return nums.Contains(original) ? FindFinalValue(nums, original * 2) : original;
+        // while (nums.Contains(original))
+        // original *= 2;
+        // return original;
     }
 }
