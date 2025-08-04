2b8987c5e5552ab5ead459c388845d926aebb469
RASHID RAHMAN <rashidr0006@gmail.com>
2025-08-04 21:04:56 +0530

    Time: 0 ms (100%), Space: 29.3 MB (34.36%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n <= 2) return n;
+
+        int first = 1, second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
